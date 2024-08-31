4873574f69b11cb27d279073d06997efe719ee3f
HARSHGUPTA3009 <HARSH30GUPTA09@GMAIL.COM>
2024-08-31 13:23:17 +0530

    try

--- game/js/utils.js
+++ game/js/utils.js
@@ -1,4 +1,4 @@
-// import { player1, player2 } from '../mainpage/controls'
+import { player1, player2 } from 'controls.js'
 function rectangularCollision({ rectangle1, rectangle2 }) {
   return (
     rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
@@ -17,9 +17,9 @@ function determineWinner({ player, enemy, timerId }) {
   if (player.health === enemy.health) {
     document.querySelector('#displayText').innerHTML = 'Tie'
   } else if (player.health > enemy.health) {
-    document.querySelector('#displayText').innerHTML = `{player1} Wins`
+    document.querySelector('#displayText').innerHTML = {player1}  `Wins`
   } else if (player.health < enemy.health) {
-    document.querySelector('#displayText').innerHTML = `{player2} Wins`
+    document.querySelector('#displayText').innerHTML = {player2} `Wins`
   }
 }
 
