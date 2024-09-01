bd4d0fe056b7f6669b418a465b4b581c588137ff
Priyanshu Patel <139453653+code-withpriyanshu@users.noreply.github.com>
2024-09-01 17:30:28 +0530

    fixed

--- controls.js
+++ controls.js
@@ -14,19 +14,21 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('Auto-play prevented:', error)
       })
   }
+})
 
-  var play = document.getElementById('play')
-  play.addEventListener('click' , function (){
-    const player1 = document.getElementById('player1Input').value
-    const player2 = document.getElementById('player2Input').value
+var play = document.getElementById('play')
+console.log(play)
+play.addEventListener('click', function () {
+  const player1 = document.getElementById('player1Input').value
+  const player2 = document.getElementById('player2Input').value
 
-    console.log(player1)
-    console.log(player2)
-    localStorage.setItem('one', player1)
-    localStorage.setItem('two', player2)
-  })
+  console.log(player1)
+  console.log(player2)
+  localStorage.setItem('one', player1)
+  localStorage.setItem('two', player2)
+})
 
-  // Capture input values when the button is clicked
+// Capture input values when the button is clicked
 //   var captureButton = document.getElementById('captureButton')
 //   captureButton.addEventListener('click', function () {
 //     const player1 = document.getElementById('player1Input').value
